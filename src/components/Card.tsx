48c07878c8fd975c83b22b5f58a60b7c7000dabf
Urvashi Chhabra <urvashi.chhabra@spenda.co>
2024-09-03 17:44:01 +0530

    first commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react";
+
+const Card = ({
+  children,
+  bg = "bg-gray-100",
+}: {
+  children: any;
+  bg: string;
+}) => {
+  return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
+};
+
+export default Card;
